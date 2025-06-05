7334b6fc08738f474dc9051911f491a629e1923f
Ramkumar <viratrambe051999@gmail.com>
2025-06-05 13:57:07 +0530

    file name changed

---
+++ environments/dev/backend.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0-beta2"
+    }
+  }
+  backend "s3" {
+  bucket = "my-terra-bucket-1"
+  key    = "environments/dev/terraform.tfstate"
+  region = "ap-south-1"
+  
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+}
+
+
+
+
